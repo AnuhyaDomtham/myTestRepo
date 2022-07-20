e6bfbd93e772dd72f75291199419b1d297b7ce3a
Anuhya Domthamsetty <anuhya.domthamsetty@ibm.com>
2022-07-20 11:43:56 +0530

    Created A.java

---
+++ A.java
@@ -0,0 +1,7 @@
+public class A
+{
+    public void display()
+    {
+	System.out.println("This is the code from Master branch 1");
+    }
+}
---
+++ B.java
@@ -0,0 +1,7 @@
+public class B
+{
+    public void display()
+    {
+	System.out.println("This is the code from Master branch 2");
+    }
+}
